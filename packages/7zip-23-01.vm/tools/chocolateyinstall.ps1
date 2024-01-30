b1e7dbd39019b0bcd6b9ccd2d2eaabb1deabcd56
Arron Patton <arron.patton@convergetp.com>
2024-01-29 22:07:17 -0600

    Update 7zip and hashcat

--- packages/7zip-23-01.vm/tools/chocolateyinstall.ps1
+++ packages/7zip-23-01.vm/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ try {
   $toolName = '7z'
   $url = 'https://www.7-zip.org/a/7z2301.exe'
   $checksum = '9B6682255BED2E415BFA2EF75E7E0888158D1AAF79370DEFAA2E2A5F2B003A59'
-  $url = 'https://www.7-zip.org/a/7z2301-x64.exe'
+  $url64 = 'https://www.7-zip.org/a/7z2301-x64.exe'
   $checksum64 = '26CB6E9F56333682122FAFE79DBCDFD51E9F47CC7217DCCD29AC6FC33B5598CD'
 
   $packageArgs = @{
