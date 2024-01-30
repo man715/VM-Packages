42949bf7799e3c365424f9b80c05e464e99e0ffa
Arron Patton <arron.patton@convergetp.com>
2024-01-29 22:04:03 -0600

    Update 7zip and hashcat

--- packages/7zip-23-01.vm/tools/chocolateyinstall.ps1
+++ packages/7zip-23-01.vm/tools/chocolateyinstall.ps1
@@ -3,12 +3,8 @@ Import-Module vm.common -Force -DisableNameChecking
 
 try {
   $toolName = '7z'
-  # $url = 'https://sourceforge.net/projects/sevenzip/files/7-Zip/15.05/7z1505.exe/download'
-  # $checksum = 'fa99d29283d9a6c501b70d2755cd06cf5bc3dd8e48acc73926b6e0f389885120'
-  $url = 'https://www.7-zip.org/a/7z2301-x64.exe'
+  $url = 'https://www.7-zip.org/a/7z2301.exe'
   $checksum = '9B6682255BED2E415BFA2EF75E7E0888158D1AAF79370DEFAA2E2A5F2B003A59'
-  # $url64 = 'https://sourceforge.net/projects/sevenzip/files/7-Zip/15.05/7z1505-x64.exe/download'
-  # $checksum64 = '6abaf04e44c87bd109df7485eb67a2d69a2e3e6e6deb9df59e5e707176c69449'
   $url = 'https://www.7-zip.org/a/7z2301-x64.exe'
   $checksum64 = '26CB6E9F56333682122FAFE79DBCDFD51E9F47CC7217DCCD29AC6FC33B5598CD'
 
