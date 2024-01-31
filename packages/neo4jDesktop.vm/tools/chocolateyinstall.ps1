7e74c41b06b736b01333ce230057b9f6b030e59b
Arron Patton <arron.patton@convergetp.com>
2024-01-31 08:39:00 -0600

    added neo4jDesktop

---
+++ packages/neo4jDesktop.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+
+$toolName = 'Neo4j Desktop'
+$category = 'Database'
+
+$exeUrl = 'https://dist.neo4j.org/neo4j-desktop/win-offline/Neo4j%20Desktop%20Setup%201.5.9.exe'
+$exeSha256 = 'F89729DBA9A8AE4694C5F6F4AB0B5B22D86D0A682530BFE123D826BA15F2F162'
+$exeName = 'Neo4j Desktop Setup 1.5.9.exe'
+$toolDir = Join-Path ${Env:RAW_TOOLS_DIR} "$toolName"
+$workingDir = Join-Path "$toolDir" "$exename"
+
+try {
+
+    # Download the exe file
+    $packageArgs = @{
+        packageName   = ${Env:ChocolateyPackageName}
+        url           = $exeUrl
+        checksum      = $exeSha256
+        checksumType  = "sha256"
+        fileFullPath  = Join-Path "${Env:USERPROFILE}\AppData\Local\Temp" ("$exeName")
+    }
+    Get-ChocolateyWebFile @packageArgs
+    $exePath = $packageArgs.fileFullPath
+    VM-Assert-Path $exePath
+
+    7z x $exePath -o"$toolDir" -y
+    # Create a shortcut
+    $executablePath = Join-Path "$workingDir" "$toolName.exe" -Resolve
+    VM-Install-Shortcut $toolName $category $executablePath -consoleApp $true -executableDir $workingDir
+} catch {
+    VM-Write-Log-Exception $_
+}
