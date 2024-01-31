e7900999867b352ee553a2b069686677d74d86f3
Arron Patton <arron.patton@convergetp.com>
2024-01-31 09:48:51 -0600

    pushing neo4j

--- packages/neo4j-desktop.vm/tools/chocolateyinstall.ps1
+++ packages/neo4j-desktop.vm/tools/chocolateyinstall.ps1
@@ -1,6 +1,9 @@
 $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
+$toolName = 'Neo4j Desktop'
+$category = 'Utilities'
+
 try {
     $exeUrl = 'https://dist.neo4j.org/neo4j-desktop/win-offline/Neo4j%20Desktop%20Setup%201.5.9.exe'
     $exeSha256 = 'F89729DBA9A8AE4694C5F6F4AB0B5B22D86D0A682530BFE123D826BA15F2F162'
@@ -14,6 +17,7 @@ try {
         fileFullPath  = Join-Path ${Env:TEMP} $installerName
     }
     $filePath = Get-ChocolateyWebFile @packageArgs
+    $executablePath = $(Join-Path ${env:ProgramFiles} "Neo4j Desktop\Neo4j Desktop.exe")
 
     VM-Assert-Path $packageArgs.fileFullPath
 
@@ -21,8 +25,9 @@ try {
     if ($rc -eq 1) {
         throw "Neo4j Desktop returned a failure exit code ($rc) for: ${Env:ChocolateyPackageName}"
     } else {
-        VM-Assert-Path $(Join-Path ${Env:PROGRAMFILES} "Neo4j Desktop\Neo4j Desktop.exe")
+        VM-Assert-Path $executablePath
     }
+    VM-Install-Shortcut $toolName $category $executablePath
 } catch {
     VM-Write-Log-Exception $_
 }
