5b284c0a513d7df4ed59183f9535cba06b6d9e5b
Arron Patton <arron.patton@convergetp.com>
2024-01-31 09:52:30 -0600

    pushing neo4j

--- packages/neo4j-desktop.vm/tools/chocolateyuninstall.ps1
+++ packages/neo4j-desktop.vm/tools/chocolateyuninstall.ps1
@@ -1,5 +1,10 @@
 $ErrorActionPreference = 'Continue'
+
+$toolName = "Neo4j Desktop"
+$category = "Utilities"
+
 Import-Module vm.common -Force -DisableNameChecking
 
 VM-Uninstall-With-Uninstaller "Neo4j Desktop" "EXE" "/S"
 
+VM-Remove-Tool-Shortcut $toolName $category
